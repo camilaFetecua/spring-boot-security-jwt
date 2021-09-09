b680614ebfea1e00ca4748184252cf8e64b3c4d3
unknown <maria.fetecua@mail.escuelaing.edu.co>
2021-09-09 15:38:14 -0500

    Funcionando

--- src/main/java/org/ada/school/controller/auth/AuthController.java
+++ src/main/java/org/ada/school/controller/auth/AuthController.java
@@ -20,52 +20,43 @@ import java.util.Date;
 import static org.ada.school.utils.Constants.CLAIMS_ROLES_KEY;
 import static org.ada.school.utils.Constants.TOKEN_DURATION_MINUTES;
 
+import static org.ada.school.utils.Constants.CLAIMS_ROLES_KEY;
+import static org.ada.school.utils.Constants.TOKEN_DURATION_MINUTES;
 @RestController
 @RequestMapping( "v1/auth" )
-public class AuthController
-{
-
-    @Value( "${app.secret}" )
+public class AuthController {
+    @Value("${app.secret}")
     String secret;
-
     private final UserService userService;
 
-    public AuthController( @Autowired UserService userService )
-    {
+    public AuthController(@Autowired UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping
-    public TokenDto login( @RequestBody LoginDto loginDto )
-    {
-        User user = userService.findByEmail( loginDto.email );
-        if ( BCrypt.checkpw( loginDto.password, user.getPasswordHash() ) )
-        {
-            return generateTokenDto( user );
-        }
-        else
-        {
+    public TokenDto login(@RequestBody LoginDto loginDto) {
+        User user = userService.findByEmail(loginDto.email);
+        if (BCrypt.checkpw(loginDto.password, user.getPasswordHash())) {
+            return generateTokenDto(user);
+        } else {
             throw new InvalidCredentialsException();
         }
-
     }
 
-    private String generateToken( User user, Date expirationDate )
-    {
+    private String generateToken(User user, Date expirationDate) {
         return Jwts.builder()
-                .setSubject( user.getId() )
-                .claim( CLAIMS_ROLES_KEY, user.getRoles() )
-                .setIssuedAt(new Date() )
-                .setExpiration( expirationDate )
-                .signWith( SignatureAlgorithm.HS256, secret )
+                .setSubject(user.getId())
+                .claim(CLAIMS_ROLES_KEY, user.getRoles())
+                .setIssuedAt(new Date())
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
 
-    private TokenDto generateTokenDto( User user )
-    {
+    private TokenDto generateTokenDto(User user) {
         Calendar expirationDate = Calendar.getInstance();
-        expirationDate.add( Calendar.MINUTE, TOKEN_DURATION_MINUTES );
-        String token = generateToken( user, expirationDate.getTime() );
-        return new TokenDto( token, expirationDate.getTime() );
+        expirationDate.add(Calendar.MINUTE, TOKEN_DURATION_MINUTES);
+        String token = generateToken(user, expirationDate.getTime());
+        return new TokenDto(token, expirationDate.getTime());
     }
 }
--- src/main/java/org/ada/school/utils/Constants.java
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -1,8 +1,10 @@
 package org.ada.school.utils;
 
 public class Constants {
-    public static String CLAIMS_ROLES_KEY = "user";
-    public static int TOKEN_DURATION_MINUTES= 80;
-    public static String COOKIE_NAME="Camila";
+    public static String COOKIE_NAME = "ada-JWT";
+    public static  String CLAIMS_ROLES_KEY = "roles";
+    public static int TOKEN_DURATION_MINUTES = 2000;
+    public static  String ADMIN_ROLE = "ADMIN";
+    public static  String USER_ROLE = "USER";
 
 }
